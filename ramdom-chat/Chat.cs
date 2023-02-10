a046d3551173e9f26c82a48fb3dbdbc4bde43f8b
Tatyana Jacques <taty.aj@gmail.com>
2023-02-10 16:18:35 -0300

    Exercicio 02

---
+++ ramdom-chat/Chat.cs
@@ -0,0 +1,12 @@
+﻿public static class Chat
+{
+    public static Dictionary<int, string> Text = new Dictionary<int, string> { { 1, "Hi" }, { 2, "Bye" } };
+    public static void ToTalk()
+    {
+        Random rand = new Random();
+        int choice = rand.Next(1, 3);
+
+        Console.WriteLine(Text[choice]);
+    }
+
+}
