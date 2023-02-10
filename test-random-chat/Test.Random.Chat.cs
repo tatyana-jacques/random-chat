fa3ef0aa858be38b70778493bc8a167db464e1ed
Tatyana Jacques <taty.aj@gmail.com>
2023-02-10 15:46:41 -0300

    Exercício 01.

---
+++ test-random-chat/Test.Random.Chat.cs
@@ -0,0 +1,15 @@
+namespace test_random_chat;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
